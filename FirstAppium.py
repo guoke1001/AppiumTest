5466ee1e9aa8291f3f174340a7d729dc253e0948
guoke1001 <tangyao1001@163.com>
2020-04-25 23:29:36 +0800

    test swipe

--- FirstAppium.py
+++ FirstAppium.py
@@ -3,6 +3,7 @@ __author__ = 'tangyao'
 
 from appium import webdriver
 from selenium.webdriver.common.by import By
+
 '''
 appium入门，简单事例
 '''
@@ -27,10 +28,44 @@ class TestAppium:
         self.driver.implicitly_wait(15)
 
     def test_phone(self):
-        self.driver.find_element(By.id,"com.xueqiu.android:id/user_profile_icon").click()
+        # self.driver.find_element(By.ID,"com.xueqiu.android:id/user_profile_icon").click()
+
+        # print(self.driver.find_element_by_xpath("//*[contains(@text,'我的')]").get_attribute("index"))
+        self.driver.find_element_by_xpath("//*[@text='我的']").click()
+        self.driver.find_element(By.XPATH,"//*[@text='手机号']").click()
+        self.driver.find_element_by_id("com.xueqiu.android:id/register_phone_number").send_keys("18612977085")
+        print(self.driver.find_element_by_id("com.xueqiu.android:id/register_phone_number").get_attribute("class"))
+
+        #获取当前activity
+        print(self.driver.current_activity)
+        print(self.driver.current_context)
+        # print(self.driver.current_window_handle)
+        # print(self.driver.current_url)
+
+
         # self.driver.find_element_by_id("com.xueqiu.android:id/user_profile_icon").click()
 
+    def testswipe(self):
+        self.driver.find_element_by_xpath("//*[@text='行情']").click()
+
+        self.driver.find_element_by_xpath("//*[@text='市场']").click()
+        size=self.driver.get_window_size()
+        width=size["width"]
+        height=size["height"]
+        self.driver.swipe(width*0.8,height*0.8,width*0.1,height*0.2,1000)
+    def test_uiaumator(self):
+        self.driver.find_element_by_xpath("//*[@text='行情']").click()
+        self.driver.find_element_by_xpath("//*[@text='市场']").click()
+
+        self.driver.find_element_by_android_uiautomator(
+            'new UiScrollable(new UiSelector().scrollable(true).instance(2)).scrollIntoView(new UiSelector().text("金通灵").instance(0));').click()
+        self.driver.find_element_by_android_uiautomator(
+            '''new UiScrollable
+            (new UiSelector().scrollable(true).instance(0)).scrollIntoView(
+            new UiSelector().text("HooBl").instance(0));''').click()
+
     def teardown(self):
-        self.driver.quit()
+        # self.driver.quit()
+        pass
 
 
